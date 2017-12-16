5b3e6d24ef66c90c5188a290693f3924a62feceb
chenkeng <chenchaohan@outlook.com>
2017-12-16 21:48:35 +0800

    change sth

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
-var sh=$.extend({},{name:"superhan"},{age:28});
+; (function($){
+    $.fn.tab=function(options){
+        var opts=$.extend({},$.fn.tab.defaults,options);
+        return this.each(function(){
+            var obj=$(this);
+            $(obj).find('.tab_header li').on("click",function(){
+                $(obj).find('.tab_)header li').removeClass('active');
 
-console.log(sh)
+            })
 
 
 
 
-$.fn.extend({},{},{});
+
+
+        })
+    }
+})(jQuery);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
