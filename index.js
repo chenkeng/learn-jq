c0fd9dc932402e4fbbd60dee4249b1e9eea71efe
superhan <chenchaohan@outlook.com>
2017-12-17 09:35:18 +0800

    change js

--- index.js
+++ index.js
@@ -1,34 +1,22 @@
 ; (function($){
     $.fn.tab=function(options){
         var opts=$.extend({},$.fn.tab.defaults,options);
+        console.log(this);//  tab  jq 对象
         return this.each(function(){
+            console.log(this);// tab js 对象
             var obj=$(this);
+            console.log($(this));// tab  jq 对象
             $(obj).find('.tab_header li').on("click",function(){
-                $(obj).find('.tab_)header li').removeClass('active');
-
+                $(obj).find('.tab_header li').removeClass('active');
+                $(this).addClass('active');
+                console.log($(this));// li 的jq 对象
+                $(obj).find('.tab_content div').hide();
+                $(obj).find('.tab_content div').eq($(this).index()).show();
             })
-
-
-
-
-
-
         })
     }
+
+    $.fn.tab.defaults={
+
+    };
 })(jQuery);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
